cc9b6a0e3b240970b5f714e418cca7f2191074ce
Mehdi Asadi <mehdiasadi63@gmail.com>
2024-09-30 13:24:35 +0330

    update input

--- components/ui/input-with-icon.tsx
+++ components/ui/input-with-icon.tsx
@@ -1,18 +1,30 @@
 import * as React from 'react'
 import { Input } from './input'
-export default function InputWithIcon() {
+import { cn } from '@/lib/utils'
+import Image from 'next/image'
+export default function InputWithIcon({
+	icon = '',
+	className = '',
+	error = '',
+	...props
+}) {
 	return (
-		<div className="flex relative">
+		<div className="grid *:col-start-1 *:row-start-1 items-center">
 			<Input
-				type="text"
-				placeholder="Text Field Empty"
-				className="px-9 py-3 opacity-50"
+				className={cn(
+					'py-3',
+					icon ? 'px-10' : 'px-4',
+					error ? 'text-red border-red' : '',
+					className,
+				)}
+				{...props}
 			/>
-			<img
-				src="/icon-arrow.svg"
-				className="absolute top-3 left-2"
-				alt="icon"
-			/>
+			{error && (
+				<p className="text-red text-body-s justify-self-end pr-4 z-10">
+					{error}
+				</p>
+			)}
+			{icon && <Image src={icon} className="ml-4 z-10" alt="icon" />}
 		</div>
 	)
 }
